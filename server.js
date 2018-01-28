a72736a491f9d27cc8e3a2268ebc5719c0452fac
slayer1ss <dagcant@gmail.com>
2018-01-28 04:03:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -42,3 +42,13 @@ cors_proxy.createServer({
 }).listen(port, host, function() {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
+
+var fs = require('fs');
+require('http').createServer(function(req, res) {
+  if (req.url === '/crossdomain.xml') {
+    fs.createReadStream('crossdomain.xml').pipe(res);
+    return;
+  }
+  // Let the server handle it
+  cors_proxy.emit('request', req, res);
+}).listen(8080); // Listen on port 8080.
