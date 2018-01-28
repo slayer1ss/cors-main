848c1e77ddb5a7791914f77c25150eb0b8e2d23e
slayer1ss <dagcant@gmail.com>
2018-01-28 04:06:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -19,8 +19,7 @@ function parseEnvList(env) {
 // Set up rate-limiting to avoid abuse of the public CORS Anywhere server.
 var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELIMIT);
 
-var cors_proxy = require('./lib/cors-anywhere');
-cors_proxy.createServer({
+var cors_proxy = require('./lib/cors-anywhere').createServer({
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
   requireHeader: [],
