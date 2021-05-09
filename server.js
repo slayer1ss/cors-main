c43a8803d4d93173eb1a06d63177f6cd04d4df37
slayer1ss <dagcant@gmail.com>
2021-05-09 21:00:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,8 @@ var cors_proxy = require('./lib/cors-anywhere').createServer({
   requireHeader: [],
   checkRateLimit: checkRateLimit,
   setHeaders:{
-     "Referer": "http://liveonscore.tv"
+     #"Referer": "http://liveonscore.tv",
+     "Referer": "https://www.streameast.live"
   },  
   removeHeaders: [
     'cookie',
