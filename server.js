2e135c1cb2877bfd4e264bf8a599a1e80bd6228c
slayer1ss <dagcant@gmail.com>
2021-05-17 20:01:22 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,14 @@
 var host = process.env.HOST || '0.0.0.0';
 var port = process.env.PORT || 8080;
 
-var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
-var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
-function parseEnvList(env) {
-  if (!env) {
-    return [];
-  }
-  return env.split(',');
-}
-
-// Set up rate-limiting to avoid abuse of the public CORS Anywhere server.
-var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELIMIT);
+function parseEnvList(env) {if (!env) {return [];}return env.split(',');}
 
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
-  originBlacklist: originBlacklist,
+  originBlacklist: [],
   originWhitelist: [],
   requireHeader: [],
-  checkRateLimit: checkRateLimit,
+  checkRateLimit: null,
   setHeaders:{    
     "Referer": ""
   },  
@@ -43,8 +33,3 @@ cors_proxy.createServer({
 }).listen(port, host, function() {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
-
-var fs = require('fs');
-require('http').createServer(function(req, res) {
-  if (req.url === '/crossdomain.xml') {fs.createReadStream('crossdomain.xml').pipe(res);return;}cors_proxy.emit('request', req, res);
-}).listen(8080);
